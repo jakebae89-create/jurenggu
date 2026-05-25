43da4e7713b57a6694b13b64a59f8cce7f9228f7
vegita89 <vegita89@daum.net>
2026-05-25 15:48:07 +0900

    주령구 PWA: 오프라인 지원 + 매니페스트 + 아이콘

---
+++ sw.js
@@ -0,0 +1,50 @@
+// 주령구 PWA 서비스 워커
+const CACHE = 'jurenggu-v1';
+
+// 앱 실행에 필요한 로컬 자산 (오프라인 대비 미리 저장)
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './vendor/three/three.module.js',
+  './vendor/three/addons/controls/OrbitControls.js',
+  './vendor/three/addons/geometries/ConvexGeometry.js',
+  './vendor/three/addons/math/ConvexHull.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 캐시 우선 + 네트워크 폴백. 받아온 응답(구글 폰트 포함)은 런타임 캐시에 저장.
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => {
+      if (hit) return hit;
+      return fetch(req).then(res => {
+        if (res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        }
+        return res;
+      }).catch(() => hit);
+    })
+  );
+});
